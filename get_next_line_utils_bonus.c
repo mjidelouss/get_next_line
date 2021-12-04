0858fb54ced687d93c428b68b01e2ff98bf0b46a
ABDELMAJID ELOUSSE <77199542+mjidelouss@users.noreply.github.com>
2021-12-04 18:57:01 +0100

    Add files via upload

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelousse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 16:34:56 by aelousse          #+#    #+#             */
+/*   Updated: 2021/12/03 22:44:59 by aelousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line_bonus.h"
+
+static void	ft_strcat(char *dest, char *src, char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		str[i] = dest[i];
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		str[i + j] = src[j];
+		j++;
+	}
+	str[i + j] = '\0';
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		total;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	total = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (total + 1));
+	if (!str)
+		return (NULL);
+	ft_strcat(s1, s2, str);
+	free(s1);
+	return (str);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == (char) c)
+			return (s);
+		s++;
+	}
+	if (c == 0)
+		return (s);
+	return (NULL);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
